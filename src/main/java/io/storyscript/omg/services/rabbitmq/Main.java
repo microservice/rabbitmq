0f79f7b199fe476cd4fb7cfd59dc9713702476e9
judepereira <contact@judepereira.com>
2019-04-30 20:56:41 +0200

    Disable automatic mongoDB connection.

--- src/main/java/io/storyscript/omg/services/rabbitmq/Main.java
+++ src/main/java/io/storyscript/omg/services/rabbitmq/Main.java
@@ -1,7 +1,11 @@
 package io.storyscript.omg.services.rabbitmq;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 /**
  * User: Jude Pereira
@@ -9,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Time: 23:21
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class,
+        MongoRepositoriesAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
